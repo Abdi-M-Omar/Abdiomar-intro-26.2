9d3bbe9ea281508edb4aa88cc511cd7bbc2e6579
Abdi Omar <abdiharbi47@gmail.com>
2026-05-26 22:13:03 -0500

    improved js file stretch goals

--- js/index.js
+++ js/index.js
@@ -38,57 +38,95 @@ for (let i = 0; i < skills.length; i++) {
 
     skillsList.appendChild(skill);
 }
-const messageForm = document.querySelector('form[name="leave_message"]');
+// Message Form
+const messageForm = document.querySelector(
+    'form[name="leave_message"]'
+);
+
 const messageSection = document.querySelector("#messages");
+
 const messageList = messageSection.querySelector("ul");
 
+// Hide messages section initially
 messageSection.style.display = "none";
 
 messageForm.addEventListener("submit", function (event) {
+
     event.preventDefault();
 
     const usersName = event.target.usersName.value;
+
     const usersEmail = event.target.usersEmail.value;
+
     const usersMessage = event.target.usersMessage.value;
+
+    // Console logs
+    console.log(usersName);
+
+    console.log(usersEmail);
+
+    console.log(usersMessage);
 
     const newMessage = document.createElement("li");
 
     newMessage.innerHTML = `
-        <a href="mailto:${usersEmail}">${usersName}</a>
+        <a href="mailto:${usersEmail}">
+            ${usersName}
+        </a>
         <span> wrote: ${usersMessage}</span>
     `;
 
+    // Edit button
     const editButton = document.createElement("button");
+
     editButton.innerText = "edit";
+
     editButton.type = "button";
 
     editButton.addEventListener("click", function () {
+
         const messageSpan = newMessage.querySelector("span");
+
         const newText = prompt("Edit your message:");
 
         if (newText !== null && newText.trim() !== "") {
+
             messageSpan.innerText = " wrote: " + newText;
+
         }
+
     });
 
+    // Remove button
     const removeButton = document.createElement("button");
+
     removeButton.innerText = "remove";
+
     removeButton.type = "button";
 
     removeButton.addEventListener("click", function () {
+
         newMessage.remove();
 
+        // Hide section if empty
         if (messageList.children.length === 0) {
+
             messageSection.style.display = "none";
+
         }
+
     });
 
     newMessage.appendChild(editButton);
+
     newMessage.appendChild(removeButton);
 
     messageList.appendChild(newMessage);
 
+    // Show section after adding message
     messageSection.style.display = "block";
 
+    // Reset form
     messageForm.reset();
+
 });
